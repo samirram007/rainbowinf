eb269c86488e17b387708b81643e981e8b6bc221
Samir Ram <samirram007@gmail.com>
2024-02-21 01:14:32 +0530

    idea update

--- resources/views/frontend/landing/banner.blade.php
+++ resources/views/frontend/landing/banner.blade.php
@@ -11,58 +11,17 @@
         height="100%" width="100%"
 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen></iframe> --}}
-             <iframe width="100%" height="100%" src="https://www.youtube.com/embed/5apTqpDZRUM?autoplay=1&loop=1&rel=0&mute=1&playlist=5apTqpDZRUM"
+             <iframe width="100%" height="100%"
+             src="https://www.youtube.com/embed/5apTqpDZRUM?autoplay=1&loop=1&rel=0&mute=1&playlist=5apTqpDZRUM&controls=0&showinfo=0&fullscreen=1"
             title="Web cover video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
     <style>
         #bg-video {}
     </style>
-    //jquery link
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script>
-        let vid = document.getElementById("bg-video");
-        vid.playbackRate = 0.5;
-        // function getPlaySpeed() {
-        //   alert(vid.playbackRate);
-        // }
 
-        // function setPlaySpeed() {
-        //   vid.playbackRate = 0.5;
-        // }
-        jQuery(function() {
-
-            // Youtube player
-            window.videoPlayer;
-
-            window.onYouTubeIframeAPIReady = function() {
-                var videoPlayerId = $('#videoPlayer').attr('data-videoid');
-                window.videoPlayer = new YT.Player('videoPlayer', {
-                    height: '1080',
-                    width: '1920',
-                    videoId: videoPlayerId,
-                    playerVars: {
-                        'controls': 0,
-                        'autoplay': 1,
-                        'mute': 1,
-                        'loop': 1,
-                        'showinfo': 0,
-                        'modestbranding': 1
-                    },
-                    events: {
-                        'onReady': onVideoPlayerReady,
-                        'onStateChange': onVideoPlayerReady
-                    }
-                });
-            }
-
-            function onVideoPlayerReady(event) {
-                videoPlayer.playVideo();
-            }
-        });
-    </script>
 
     <div class="video-overlay header-text">
-        <div class="caption">
+        <div class="caption d-none">
             {{-- <h6>Executive Summary</h6> --}}
             <h2><em>Trade where we Trade </em>
                 <h4 class="text-white font-weight-bold" style="text-shadow: 1px 3px 5px #0d173e;">Founded in the year
